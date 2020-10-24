af31dfbdb83fbfb2f08a7cf63ca9397bd28fe295
fsd66 <fsd66@rocketmail.com>
2020-10-23 23:21:27 -0400

    revamp again

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+import Header from "./components/Header";
+import GithubLink from "./components/GithubLink";
+import WorkRequest from "./components/WorkRequest";
+import KoFiLink from "./components/KoFiLink";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <div>
+        <p>Please visit my <GithubLink /></p>
+      </div>
+      <div>
+        <div>Enjoy my work?</div>
+        <KoFiLink />
+      </div>
+      <WorkRequest />
+    </div>
+  );
+}
+
+export default App;
